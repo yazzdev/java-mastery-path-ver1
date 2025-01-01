a0cb60fab01f2153cd5ce54564a27f4e74219170
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-01-01 15:41:32 +0700

    Iterable & Iteroator

---
+++ src/belajar/java/collection/IterableApp.java
@@ -0,0 +1,21 @@
+package belajar.java.collection;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class IterableApp {
+  public static void main(String[] args) {
+   Iterable<String> names = List.of("Dyaz", "Amrullah", "Belajar", "Java");
+    for (var name : names) {
+      System.out.println(name);
+    }
+
+    System.out.println("--- ITERATOR ---");
+    Iterator<String> iterator = names.iterator();
+
+    while (iterator.hasNext()) {
+      String name = iterator.next();
+      System.out.println(name);
+    }
+  }
+}
