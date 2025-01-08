2cd73e70d659772e656a3b5452d93681975b8ea2
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-01-08 16:51:14 +0700

    Immutable Map

---
+++ src/belajar/java/collection/SortedMapApp.java
@@ -0,0 +1,32 @@
+package belajar.java.collection;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class SortedMapApp {
+  public static void main(String[] args) {
+
+    Comparator<String> stringComparator = new Comparator<String>() {
+      @Override
+      public int compare(String o1, String o2) {
+        return o2.compareTo(o1);
+      }
+    };
+
+    SortedMap<String, String> sortedMap = new TreeMap<>(stringComparator);
+
+    sortedMap.put("Dyaz", "Dyaz");
+    sortedMap.put("Amrullah", "Amrullah");
+
+    for (var key : sortedMap.keySet()) {
+      System.out.println(key);
+    }
+
+    SortedMap<String, String> empty = Collections.emptySortedMap();
+    SortedMap<String, String> immutable = Collections.unmodifiableSortedMap(sortedMap);
+
+    // immutable.put("Dyaz", "Ups"); //Error: immutable
+  }
+}
