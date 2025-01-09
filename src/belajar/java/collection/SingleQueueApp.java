a30afbb0ac21df7b6cab1c3107860d278d4e284e
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-01-09 14:01:30 +0700

    Abstract Collection

---
+++ src/belajar/java/collection/SingleQueueApp.java
@@ -0,0 +1,26 @@
+package belajar.java.collection;
+
+import belajar.java.collection.collections.SingleQueue;
+
+import java.util.Queue;
+
+public class SingleQueueApp {
+  public static void main(String[] args) {
+
+    Queue<String> queue = new SingleQueue<>();
+    System.out.println(queue.size());
+
+    System.out.println(queue.offer("Dyaz"));
+    System.out.println(queue.offer("Amrullah"));
+    System.out.println(queue.offer("Amrullah"));
+
+    System.out.println(queue.size());
+
+    System.out.println(queue.peek());
+    System.out.println(queue.poll());
+    System.out.println(queue.poll());
+    System.out.println(queue.poll());
+
+    System.out.println(queue.size());
+  }
+}
