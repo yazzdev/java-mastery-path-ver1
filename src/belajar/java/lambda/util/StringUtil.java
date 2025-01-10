b32656b8c9a80c0dc9f5d03aac95e9521dcbb376
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-01-10 21:51:12 +0700

    Method Reference

---
+++ src/belajar/java/lambda/util/StringUtil.java
@@ -0,0 +1,14 @@
+package belajar.java.lambda.util;
+
+public class StringUtil {
+
+  public static boolean isLowerCase(String value){
+    for(var c : value.toCharArray()){
+      if (!Character.isLowerCase(c)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
